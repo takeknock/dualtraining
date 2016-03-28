d269ebb76c330c97e8739ad7d1831cd175c79e22
tkhr <mmatthew43@outlook.jp>
2016-03-29 08:17:09 +0900

    add stripping cap vols

--- Dual/Dual/main.cpp
+++ Dual/Dual/main.cpp
@@ -46,14 +46,14 @@ int main()
     discountFactor[5] = 0.96; // 1Y 
     discountFactor[6] = 0.94; // 1Y6M
     discountFactor[7] = 0.93; // 2Y
-    discountFactor[8] = 0.92; // 3Y
-    discountFactor[9] = 0.91; // 4Y 
-    discountFactor[10] = 0.90; // 5Y
-    discountFactor[11] = 0.89; // 6Y
-    discountFactor[12] = 0.889; // 7Y
-    discountFactor[13] = 0.88; // 8Y
-    discountFactor[14] = 0.87; // 9Y
-    discountFactor[15] = 0.86; // 10Y
+    discountFactor[8] = 0.92; // 2Y6M
+    discountFactor[9] = 0.91; // 3Y 
+    discountFactor[10] = 0.90; // 3Y6M
+    discountFactor[11] = 0.89; // 4Y
+    discountFactor[12] = 0.889; // 5Y6M
+    discountFactor[13] = 0.88; // 6Y
+    discountFactor[14] = 0.87; // 6Y6M
+    discountFactor[15] = 0.86; // 7Y
 
     ublas::vector<double> forwardLibor(9);
     // Let day count convention be ACT360, 
@@ -156,7 +156,7 @@ int main()
 	//cap1Y : 0.000792079
 	//derivative of cap1Y with forwardLibor6M : -0.226536
 
-   // ATM Volatilities
+   // ATM Cap Volatilities quoted in market
     ublas::vector<double> capVolatilities(7);
     capVolatilities[0] = 1.86;// 1Y
     capVolatilities[1] = 1.38;// 2Y
@@ -166,21 +166,94 @@ int main()
     capVolatilities[5] = 0.55;// 7Y
     capVolatilities[6] = 0.43;// 10Y
 
-    // from cap vol to cap price
-    boost::math::normal norm(0,1);
-    
-    const double libor1Y = 1.0 * (1.0 / discountFactor[5] - 1.0);
+    boost::math::normal norm(0, 1);
     const double capRate = 0.1;
-    const double cap1Yd1 = 
-        (log(libor1Y / capRate) + 0.5 * capVolatilities[0] * capVolatilities[0] * 1.0)
-        / (capVolatilities[0] * 1.0);
-    const double cap1Yd2 =
-        (log(libor1Y / capRate) - 0.5 * capVolatilities[0] * capVolatilities[0] * 1.0)
-        / (capVolatilities[0] * 1.0);
 
-    const double capPrice1Y = 1.0 * discountFactor[5] * (libor1Y * cdf(norm, cap1Yd1) - capRate * cdf(norm, cap1Yd2));
-    std::cout << "cap volatility 1Y :" << capVolatilities[0] << std::endl;
-    std::cout << "capPrice1Y :" << capPrice1Y << std::endl;
+    //// from cap vol to cap price
+    //// 1Y
+    //
+    //
+    //const double libor1Y = 1.0 * (1.0 / discountFactor[5] - 1.0);
+
+    //
+    //const double cap1Yd1 = 
+    //    (log(libor1Y / capRate) + 0.5 * capVolatilities[0] * capVolatilities[0] * 1.0)
+    //    / (capVolatilities[0] * 1.0);
+    //const double cap1Yd2 =
+    //    (log(libor1Y / capRate) - 0.5 * capVolatilities[0] * capVolatilities[0] * 1.0)
+    //    / (capVolatilities[0] * 1.0);
+
+    //const double capPrice1Y = 1.0 * discountFactor[5] * (libor1Y * cdf(norm, cap1Yd1) - capRate * cdf(norm, cap1Yd2));
+    //std::cout << "cap volatility 1Y :" << capVolatilities[0] << std::endl;
+    //std::cout << "capPrice1Y :" << capPrice1Y << std::endl;
+
+    //// 2Y
+    //const double libor2Y = 2.0 * (1.0 / discountFactor[7] - 1.0);
+
+    //const double cap2Yd1 =
+    //    (std::log(libor2Y / capRate) + 0.5 * capVolatilities[1] * capVolatilities[1] * 2.0)
+    //    / (capVolatilities[1] * std::sqrt(2.0));
+    //const double cap2Yd2 =
+    //    (std::log(libor2Y / capRate) - 0.5 * capVolatilities[1] * capVolatilities[1] * 2.0)
+    //    / (capVolatilities[1] * std::sqrt(2.0));
+
+    //const double capPrice2Y = 2.0 * discountFactor[7] * (libor2Y * cdf(norm, cap2Yd1) - capRate * cdf(norm, cap2Yd2));
+    //std::cout << "cap volatility 2Y :" << capVolatilities[1] << std::endl;
+    //std::cout << "capPrice2Y :" << capPrice2Y << std::endl;
+
+    //// 3Y
+    //const double libor3Y = 3.0 * (1.0 / discountFactor[9] - 1.0);
+
+    //const double cap3Yd1 =
+    //    (std::log(libor3Y / capRate) + 0.5 * capVolatilities[2] * capVolatilities[2] * 3.0)
+    //    / (capVolatilities[2] * std::sqrt(3.0));
+    //const double cap3Yd2 =
+    //    (std::log(libor3Y / capRate) - 0.5 * capVolatilities[2] * capVolatilities[2] * 3.0)
+    //    / (capVolatilities[2] * std::sqrt(3.0));
+
+    //const double capPrice3Y = 3.0 * discountFactor[9] * (libor3Y * cdf(norm, cap3Yd1) - capRate * cdf(norm, cap3Yd2));
+    //std::cout << "cap volatility 3Y :" << capVolatilities[2] << std::endl;
+    //std::cout << "capPrice3Y :" << capPrice3Y << std::endl;
+
+    // assume piecewise constant volatility
+    ublas::vector<double> capletVolatilities(4);
+    capletVolatilities[0] = capVolatilities[0]; // 6M
+    capletVolatilities[1] = capVolatilities[0]; // 1Y
+    
+    // use 2Y cap volatility for 1Y6M cap pricing
+    const double libor1Y6M = 1.5 * (1.0 / discountFactor[6] - 1.0);
+    const double cap1Y6Md1 = 
+        (std::log(libor1Y6M / capRate) + 0.5 * capVolatilities[1] * capVolatilities[1] * 1.5)
+        / (capVolatilities[1] * std::sqrt(1.5));
+
+    const double cap1Y6Md2 =
+        (std::log(libor1Y6M / capRate) - 0.5 * capVolatilities[1] * capVolatilities[1] * 1.5)
+        / (capVolatilities[1] * std::sqrt(1.5));
+
+    const double capPrice1Y6M = 1.5 * discountFactor[6] * (libor1Y6M * cdf(norm, cap1Y6Md1) - capRate * cdf(norm, cap1Y6Md2));
+    std::cout << "cap volatility 1Y6M :" << capVolatilities[1] << std::endl;
+    std::cout << "capPrice1Y6M :" << capPrice1Y6M << std::endl;
+
+    // black(price) = implied vol
+    for (std::size_t i = 0; i < 20; ++i) {
+        break;
+    }
+
+    //def newtonRap(cp, price, s, k, t, rf) :
+    //    v = sqrt(2 * pi / t)*price / s
+    //    print "initial volatility: ", v
+    //    for i in range(1, 100) :
+    //        d1 = (log(s / k) + (rf + 0.5*pow(v, 2))*t) / (v*sqrt(t))
+    //        d2 = d1 - v*sqrt(t)
+    //        vega = s*norm.pdf(d1)*sqrt(t)
+    //        price0 = cp*s*norm.cdf(cp*d1) - cp*k*exp(-rf*t)*norm.cdf(cp*d2)
+    //        v = v - (price0 - price) / vega
+    //        print "price, vega, volatility\n", (price0, vega, v)
+    //        if abs(price0 - price) < 1e-25 :
+    //break
+    //    return v
+
+
     double ddd;
     std::cin >> ddd;
 
