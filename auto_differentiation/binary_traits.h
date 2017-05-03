7bbe4f9f291e398f0de163607e7a984c0036f91e
mmatthew <takeknock@gmail.com>
2017-05-03 10:34:27 +0900

    add binary traits

--- auto_differentiation/binary_traits.h
+++ auto_differentiation/binary_traits.h
@@ -1,1 +1,20 @@
 #pragma once
+
+struct true_type {};
+struct false_type {};
+
+template<typename E1, typename E2>
+struct binary_traits {
+public:
+    // binary_traits<E1, E2>::type 
+    typedef double binary_traits<int, double>::value_type;
+    typedef double binary_traits<double, double>::value_type;
+    typedef double binary_traits<double, int>::value_type;
+
+
+    
+private:
+
+};
+
+
