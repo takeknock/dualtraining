95a0655422cc0ee18a70589a7979c44f15ec0ca2
mmatthew <takeknock@gmail.com>
2017-05-03 10:45:52 +0900

    add binary operators and change unary operators

--- auto_differentiation/dual_number.h
+++ auto_differentiation/dual_number.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "binary_traits.h"
+
 namespace ad
 {
     template<typename E, typename D>
@@ -74,59 +76,67 @@ namespace ad
             return this_type(*this) /= rhs;
         }
 
-        // operators for any rhs types
-        template<typename ER, typename DR>
-        inline this_type& operator+=(const dual_number<ER, DR>& rhs)
+        // unitary operators for any rhs types
+        template<typename E, typename D, typename ER, typename DR>
+        inline dual_number<binary_traits<E, ER>::value_type, binary_traits<D, DR>::value_type>
+            operator+=(const dual_number<ER, DR>& rhs)
         {
             _e += rhs.e();
             _d += rhs.d();
             return this*;
         }
 
-        template<typename ER, typename DR>
-        inline this_type& operator-=(const dual_number<ER, DR>& rhs)
+        template<typename E, typename D, typename ER, typename DR>
+        inline dual_number<binary_traits<E, ER>::value_type, binary_traits<D, DR>::value_type>&
+            operator-=(const dual_number<ER, DR>& rhs)
         {
             _e -= rhs.e();
             _d -= rhs.d();
             return this*;
         }
 
-        template<typename ER, typename DR>
-        inline this_type& operator*=(const dual_number<ER, DR>& rhs)
+        template<typename E, typename D, typename ER, typename DR>
+        inline dual_number<binary_traits<E, ER>::value_type, binary_traits<D, DR>::value_type>&
+            operator*=(const dual_number<ER, DR>& rhs)
         {
             _e *= rhs.e();
             _d = _d * rhs.e() + _e * rhs.d();
             return this*;
         }
 
-        template<typename ER, typename DR>
-        inline this_type& operator/=(const dual_number<ER, DR>& rhs)
+        template<typename E, typename D, typename ER, typename DR>
+        inline dual_number<binary_traits<E, ER>::value_type, binary_traits<D, DR>::value_type>&
+            operator/=(const dual_number<ER, DR>& rhs)
         {
             _e /= rhs.e();
             _d = (_d * rhs.e() - _e * rhs.d()) / (rhs.e() * rhs.e());
             return this*;
         }
 
-        template<typename ER, typename DR>
-        inline this_type& operator +(const dual_number<ER, DR>& rhs)
+        template<typename E, typename D, typename ER, typename DR>
+        inline dual_number<binary_traits<E, ER>::value_type, binary_traits<D, DR>::value_type>&
+            operator +(const dual_number<ER, DR>& rhs)
         {
             return this_type(*this) += rhs;
         }
 
-        template<typename ER, typename DR>
-        inline this_type& operator -(const dual_number<ER, DR>& rhs)
+        template<typename E, typename D, typename ER, typename DR>
+        inline dual_number<binary_traits<E, ER>::value_type, binary_traits<D, DR>::value_type>&
+            operator -(const dual_number<ER, DR>& rhs)
         {
             return this_type(*this) -= rhs;
         }
 
-        template<typename ER, typename DR>
-        inline this_type& operator *(const dual_number<ER, DR>& rhs)
+        template<typename E, typename D, typename ER, typename DR>
+        inline dual_number<binary_traits<E, ER>::value_type, binary_traits<D, DR>::value_type>&
+            operator *(const dual_number<ER, DR>& rhs)
         {
             return this_type(*this) *= rhs;
         }
 
-        template<typename ER, typename DR>
-        inline this_type& operator /(const dual_number<ER, DR>& rhs)
+        template<typename E, typename D, typename ER, typename DR>
+        inline dual_number<binary_traits<E, ER>::value_type, binary_traits<D, DR>::value_type>&
+            operator /(const dual_number<ER, DR>& rhs)
         {
             return this_type(*this) /= rhs;
         }
@@ -145,4 +155,42 @@ namespace ad
         D _d;
 
     };
+
+
+    // binary operators for any rhs types
+    template<typename E, typename D, typename ER, typename DR>
+    inline dual_number<binary_traits<E, ER>::value_type, binary_traits<D, DR>::value_type>
+        operator+(const dual_number<E, D>& lhs, const dual_number<ER, DR>& rhs)
+    {
+        binary_traits<E, ER>::value_type e = lhs.e() + rhs.e();
+        binary_traits<D, DR>::value_type d = lhs.d() + rhs.d();
+        return dual_number<binary_traits<E, ER>::value_type, binary_traits<D, DR>::value_type>(e, d);
+    }
+
+    template<typename E, typename D, typename ER, typename DR>
+    inline dual_number<binary_traits<E, ER>::value_type, binary_traits<D, DR>::value_type>&
+        operator-(const dual_number<E, D>& lhs, const dual_number<ER, DR>& rhs)
+    {
+        binary_traits<E, ER>::value_type e = lhs.e() - rhs.e();
+        binary_traits<D, DR>::value_type d = lhs.d() - rhs.d();
+        return dual_number<binary_traits<E, ER>::value_type, binary_traits<D, DR>::value_type>(e, d);
+    }
+
+    template<typename E, typename D, typename ER, typename DR>
+    inline dual_number<binary_traits<E, ER>::value_type, binary_traits<D, DR>::value_type>&
+        operator*(const dual_number<E, D>& lhs, const dual_number<ER, DR>& rhs)
+    {
+        binary_traits<E, ER>::value_type e = lhs.e() * rhs.e();
+        binary_traits<D, DR>::value_type d = lhs.d() * rhs.e() + lhs.e() * rhs.d();
+        return dual_number<binary_traits<E, ER>::value_type, binary_traits<D, DR>::value_type>(e, d);
+    }
+
+    template<typename E, typename D, typename ER, typename DR>
+    inline dual_number<binary_traits<E, ER>::value_type, binary_traits<D, DR>::value_type>&
+        operator/(const dual_number<E, D>& lhs, const dual_number<ER, DR>& rhs)
+    {
+        binary_traits<E, ER>::value_type e = lhs.e() / rhs.e();
+        binary_traits<D, DR>::value_type d = (lhs.d() * rhs.e() - lhs.e() * rhs.d()) / (rhs.e() * rhs.e());
+        return dual_number<binary_traits<E, ER>::value_type, binary_traits<D, DR>::value_type>(e, d);
+    }
 }
